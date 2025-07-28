943a98f2c8df8d75367e672a36a0605f8cdc543c
aryaman <aryamanbohra@jklu.edu.in>
2025-07-28 09:57:20 +0530

    Switch from Chroma to FAISS in smart_rag.py

--- core/smart_rag.py
+++ core/smart_rag.py
@@ -1,17 +1,17 @@
 import os
 from core.llm_ollama import query_local_llm
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 
 embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 def get_final_response(query: str) -> str:
-    if os.path.exists("uploaded_file_vectordb"):
+    if os.path.exists("uploaded_file_vectordb/index.faiss"):
         print("📂 Using uploaded file context...")
-        vectordb = Chroma(persist_directory="uploaded_file_vectordb", embedding_function=embedding)
+        vectordb = FAISS.load_local("uploaded_file_vectordb", embedding_function=embedding)
     else:
         print("🌐 Using default vectordb...")
-        vectordb = Chroma(persist_directory="./vectordb", embedding_function=embedding)
+        vectordb = FAISS.load_local("vectordb", embedding_function=embedding)
 
     results = vectordb.similarity_search(query, k=4)
     context = "\n\n".join([r.page_content for r in results])
